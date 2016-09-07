82dcc521e0c65f6b75da8a4a3e54f49ff77dd5c7
Diana Zhou <yuz302@ucsd.edu>
2016-09-07 16:50:22 -0400

    urls
    视图函数

---
+++ Andon/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello world")
+
+def curr_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s. </body></html>" % now
+    return HttpResponse(html)
--- AndonPage/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def hello(request): #视图函数
-    return HttpResponse("Hello world")
