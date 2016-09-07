5a3a9f5d70f4d04a75b2bbb33be9f1a77f0284b7
Diana Zhou <yuz302@ucsd.edu>
2016-09-07 15:00:18 -0400

    models table

--- Andon/andon/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=200)
-    product_batch_number = models.CharField(max_length=200)
---
+++ AndonPage/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    batch_number = models.CharField(max_length=200)
+
+    def __init__(self, name, batchnumber):
+        self.name = name
+        self.batch_number = batchnumber
+
+    def get_name(self):
+        return self.name
+
+    def get_batch_number(self):
+        return self.batch_number
+
+x = Product("PH1234", "12345678")
+print(x.get_batch_number())
