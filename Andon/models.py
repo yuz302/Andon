34d799efd93b4a0a06daa721b5b86282b9d60bb0
Diana Zhou <yuz302@ucsd.edu>
2016-09-14 20:24:27 -0400

    css todo; html blocks; static files; js tomorrow

--- Andon/models.py
+++ Andon/models.py
@@ -4,6 +4,7 @@ from django.db import models
 import urllib2
 import re
 
+"""
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -21,29 +22,7 @@ class Product(models.Model):
 
 x = Product("PH1234", "12345678")
 print(x.get_batch_number())
-
+"""
 
 # below is the web scraping logic
 
-def getHtml(url):
-    response = urllib2.urlopen(url)
-    html = response.read()
-    return html
-
-
-def getProductName(html):
-    prod_re = r'<p id="prod">(.*?)</p>'
-    prod_match = re.findall(prod_re, html,re.S|re.M)
-    return prod_match
-
-
-def getProductBatchNumber(html):
-    prod_re = r'<p id="prodbatch">(.*?)</p>'
-    prod_match = re.findall(prod_re, html, re.S | re.M)
-    return prod_match
-
-if __name__ == '__main__':
-    url = 'http://10.10.10.123/user/andon.htm'
-    html = getHtml(url)
-    print getProductName(html)[0]
-    print getProductBatchNumber(html)[0]
--- Andon/views.py
+++ Andon/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.template import Template, Context
-
-from Andon.models import Product
+import urllib2
+import re
 import datetime
 
 # Create your views here.
@@ -11,9 +11,9 @@ import datetime
 def index(request):
 
     template = loader.get_template('Andon/index.html')
-    context = Context({"prod_name": "AB1234", "prod_batch_num": "87654321"})
+    context = Context({"prod_name": getProductName(getHtml('http://10.10.10.123/user/andon.htm'))[0], "prod_batch_num": getProductBatchNumber(getHtml('http://10.10.10.123/user/andon.htm'))[0]})
 
-    context2 = Context({"prod_name": "AB1234", "prod_batch_num": "87654321"}) # one template, multiple contexts
+    #context2 = Context({"prod_name": "AB1234", "prod_batch_num": "87654321"}) # one template, multiple contexts
     return HttpResponse(template.render(context))   # the template after rendering
     # return render(request, 'Andon/index.html', context)
 
@@ -21,3 +21,27 @@ def curr_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s. </body></html>" % now
     return HttpResponse(html)
+
+
+def getHtml(url):
+    response = urllib2.urlopen(url)
+    html = response.read()
+    return html
+
+
+def getProductName(html):
+    prod_re = r'<p id="prod">(.*?)</p>'
+    prod_match = re.findall(prod_re, html,re.S|re.M)
+    return prod_match
+
+
+def getProductBatchNumber(html):
+    prod_re = r'<p id="prodbatch">(.*?)</p>'
+    prod_match = re.findall(prod_re, html, re.S | re.M)
+    return prod_match
+
+if __name__ == '__main__':
+    url = 'http://10.10.10.123/user/andon.htm'
+    html = getHtml(url)
+    productname = getProductName(html)[0]
+    productbatchnumber = getProductBatchNumber(html)[0]
